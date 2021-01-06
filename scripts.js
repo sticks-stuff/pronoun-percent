bd1f142bf4220191002947b77455cb1c603007c6
Thestickman391 <stickman391@gmail.com>
2021-01-06 19:18:44 +1300

    Initial Commit

---
+++ scripts.js
@@ -0,0 +1,103 @@
+const userAction = async (username) => {
+  document.getElementById("error-message").style.opacity = "0";
+  document.getElementById("input-container-container").style.transform = "translate(0%, 45vh)";
+  document.getElementsByClassName("infoText")[0].style.position = "absolute";
+  document.getElementById("displayContainerContainer").style.opacity = "0";
+  // document.getElementById("displayContainerContainer").style.display = "none";
+  document.getElementById("displayContainer").innerHTML = "<div id='heHim'>						<div class='header'>							He/Him						</div>						<div class='stats'>							21% <br> Total: 30						</div>						<div class='list'>						</div>					</div>					<div id='heThey'>						<div class='header'>							He/They						</div>						<div class='stats'>							21% <br> Total: 30						</div>						<div class='list'>						</div>					</div>					<div id='sheHer'>						<div class='header'>							She/Her						</div>						<div class='stats'>							21% <br> Total: 30						</div>						<div class='list'>						</div>					</div>					<div id='sheThey'>						<div class='header'>							She/They						</div>						<div class='stats'>							21% <br> Total: 30						</div>						<div class='list'>						</div>					</div>					<div id='theyThem'>						<div class='header'>							They/Them						</div>						<div class='stats'>							21% <br> Total: 30						</div>						<div class='list'>						</div>					</div>"
+  //^ listen do u think i give a shit anymore? 
+  const response = await fetch(window.location.origin + ':5000/api/' + username, {
+  // const response = await fetch('http://localhost:8080/api/' + 'Thestickman391' + '.json', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+  const myJson = await response.json(); //extract JSON from the http response
+  console.log(myJson);
+  if(myJson.twitterReply != null)
+  {
+	console.log(myJson.twitterReply.errors[0].message);
+	document.getElementById("error-message").innerHTML = myJson.twitterReply.errors[0].message;
+	document.getElementById("error-message").style.opacity = "1";
+  } else {
+	  // do something with myJson
+	  // console.log(myJson[0].sheThey.length);
+	  
+	  var heHim = myJson[0].heHim ?? 0;
+	  var heThey = myJson[0].heThey ?? 0;
+	  var sheThey = myJson[0].sheThey ?? 0;
+	  var sheHer = myJson[0].sheHer ?? 0;
+	  var theyThem = myJson[0].theyThem ?? 0;
+	  var nothing = myJson[0].nothing ?? 0;  
+	  
+	  var heHimLength = heHim.length ?? 0;
+	  var heTheyLength = heThey.length ?? 0;
+	  var sheTheyLength = sheThey.length ?? 0;
+	  var sheHerLength = sheHer.length ?? 0;
+	  var theyThemLength = theyThem.length ?? 0;
+	  var nothingLength = nothing.length ?? 0;
+	  
+	  var totalFollowed = heHimLength + heTheyLength + sheTheyLength + sheHerLength + theyThemLength + nothingLength;
+	  console.log(totalFollowed);
+	  if((totalFollowed - nothingLength) <= 0)
+	  {
+		document.getElementById("error-message").innerHTML = "No data to display!";
+		document.getElementById("error-message").style.opacity = "1";
+	  } else {
+	  
+		   var fruits = [heHim, heThey, sheThey, sheHer, theyThem];
+		   var fruitStrings = ["heHim", "heThey", "sheThey", "sheHer", "theyThem"];
+		   var fruitsLength = [heHimLength, heTheyLength, sheTheyLength, sheHerLength, theyThemLength];
+		  
+		  
+		  for (var i = 0; i < fruits.length; i++) {
+			  document.getElementById(fruitStrings[i]).getElementsByClassName("stats")[0].innerHTML = ((fruitsLength[i] / totalFollowed) * 100).toFixed(1) + '%<br>Total: ' + fruitsLength[i];
+		  }
+		  
+		  for (var i = 0; i < fruits.length; i++) {
+			  for (var j = 0; j < (fruitsLength[i]); j++) {
+				item = document.createElement('div');
+				item.className = fruits[i][j].screen_name;
+				// item.style.width = "338px";
+					
+				document.getElementById(fruitStrings[i]).getElementsByClassName("list")[0].appendChild(item);
+				
+				var usernameClass = document.getElementsByClassName(fruits[i][j].screen_name)[0];
+				
+				var pfp = document.createElement("div");
+				pfp.className = "pfp";
+				// usernameClass.appendChild(pfp);
+				
+				var img = document.createElement("img");
+				img.src = fruits[i][j].profile_image_url_https;
+				
+				usernameClass.appendChild(img);
+				
+				var div = document.createElement("div");
+				div.innerHTML = fruits[i][j].screen_name;
+				div.className = "name";
+				
+				usernameClass.appendChild(div);	
+				
+				//document.getElementsByClassName('NeilCicOOC')[0].appendChild(name);
+			  }
+		  }
+		  finished();
+	  }
+  }
+}
+
+function finished() {
+	document.getElementById("input-container-container").style.transform = "translate(0%, 0%)";
+	document.getElementById("displayContainerContainer").style.display = "flex";
+	setTimeout(function(){ 
+		document.getElementsByClassName("infoText")[0].style.position = "";
+		document.getElementById("displayContainerContainer").style.opacity = "1";
+	}, 1000);
+}
+
+// userAction('Thestickman391');
+document.getElementsByClassName("infoText")[0].style.position = "absolute";
+document.getElementById("displayContainerContainer").style.display = "none";
+
---
+++ server.js
@@ -0,0 +1,201 @@
+require('dotenv').config();
+
+const Twit = require('twit')
+const tokens = {
+  consumer_key:        process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret:     process.env.TWITTER_CONSUMER_SECRET,
+  access_token:        process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  //timeout_ms: 60 * 1000
+}
+ 
+const getTwitterFollowers = require('get-twitter-followers')
+
+var heHim = [
+  "he/him",
+  "he / him",
+  "he | him",
+  "he|him",  
+  "him/he",
+  "him / he",
+  "him | he",
+  "him|he",
+];
+
+var heThey = [
+  "he/they",
+  "he / they",
+  "he | they",
+  "he|they",  
+  "they/he",
+  "they / he",
+  "they | he",
+  "they|he",
+];
+
+var sheHer = [
+  "she/her",
+  "she / her",
+  "she | her",
+  "she|her",  
+  "her/she",
+  "her / she",
+  "her | she",
+  "her|she",
+];
+
+var sheThey = [
+  "she/they",
+  "| she/they",
+  "she / they",
+  "she | they",
+  "she|they",  
+  "they/she",
+  "they / she",
+  "they | she",
+  "they|she",
+];
+
+var theyThem = [
+  "they/them",
+  "they / them",
+  "they | them",
+  "they|them",
+];
+
+let express = require('express') 
+const app = express();
+var cors = require('cors')
+app.use(cors())
+
+app.get('/api/:username', (req, res) => {
+  var apiResponse  = [];
+  var heHimCount = [];
+  var heTheyCount = [];
+  var sheHerCount = [];
+  var sheTheyCount = [];
+  var theyThemCount = [];
+  var nothingCount = [];
+  getTwitterFollowers(tokens, req.params.username).then(followers => {
+	  for (var i = 0; i < followers.length; i++) {
+			var bio = followers[i].description.toLowerCase();
+			var location = followers[i].location.toLowerCase();
+			
+			//this is awful LOL
+			if ( heHim.some(substring=>bio.includes(substring)) ) {
+				heHimCount.push(
+				{
+					screen_name: followers[i].screen_name, 
+					name: followers[i].name, 
+					profile_image_url_https: followers[i].profile_image_url_https.replace("_normal", "_400x400")
+				});
+			}		
+			else if ( heThey.some(substring=>bio.includes(substring)) && !sheThey.some(substring=>bio.includes(substring)) ) { //bad hardcoded fix to fix she/theys getting mis-identified as he/theys
+				heTheyCount.push(
+				{
+					screen_name: followers[i].screen_name, 
+					name: followers[i].name, 
+					profile_image_url_https: followers[i].profile_image_url_https.replace("_normal", "_400x400")
+				});
+			}
+			else if ( sheHer.some(substring=>bio.includes(substring)) ) {
+				sheHerCount.push(
+				{
+					screen_name: followers[i].screen_name, 
+					name: followers[i].name, 
+					profile_image_url_https: followers[i].profile_image_url_https.replace("_normal", "_400x400")
+				});
+			}
+			else if ( sheThey.some(substring=>bio.includes(substring)) ) {
+				sheTheyCount.push(
+				{
+					screen_name: followers[i].screen_name, 
+					name: followers[i].name, 
+					profile_image_url_https: followers[i].profile_image_url_https.replace("_normal", "_400x400")
+				});
+			}
+			else if ( theyThem.some(substring=>bio.includes(substring)) ) {
+				theyThemCount.push(
+				{
+					screen_name: followers[i].screen_name, 
+					name: followers[i].name, 
+					profile_image_url_https: followers[i].profile_image_url_https.replace("_normal", "_400x400")
+				});
+			}
+			else if ( heHim.some(substring=>location.includes(substring)) ) {
+				heHimCount.push(
+				{
+					screen_name: followers[i].screen_name, 
+					name: followers[i].name, 
+					profile_image_url_https: followers[i].profile_image_url_https.replace("_normal", "_400x400")
+				});
+			}		
+			else if ( heThey.some(substring=>location.includes(substring)) ) {
+				heTheyCount.push(
+				{
+					screen_name: followers[i].screen_name, 
+					name: followers[i].name, 
+					profile_image_url_https: followers[i].profile_image_url_https.replace("_normal", "_400x400")
+				});
+			}
+			else if ( sheHer.some(substring=>location.includes(substring)) ) {
+				sheHerCount.push(
+				{
+					screen_name: followers[i].screen_name, 
+					name: followers[i].name, 
+					profile_image_url_https: followers[i].profile_image_url_https.replace("_normal", "_400x400")
+				});
+			}
+			else if ( sheThey.some(substring=>location.includes(substring)) ) {
+				sheTheyCount.push(
+				{
+					screen_name: followers[i].screen_name, 
+					name: followers[i].name, 
+					profile_image_url_https: followers[i].profile_image_url_https.replace("_normal", "_400x400")
+				});
+			}
+			else if ( theyThem.some(substring=>location.includes(substring)) ) {
+				theyThemCount.push(
+				{
+					screen_name: followers[i].screen_name, 
+					name: followers[i].name, 
+					profile_image_url_https: followers[i].profile_image_url_https.replace("_normal", "_400x400")
+				});
+			}
+			else {
+				nothingCount.push(followers[i].screen_name);
+			}
+			
+		}
+		console.log(heHimCount.length);
+		console.log(heTheyCount.length);
+		console.log(sheHerCount.length);
+		console.log(sheTheyCount.length);
+		console.log(theyThemCount);
+		console.log(nothingCount.length);
+		apiResponse.push(
+		{
+			heHim: heHimCount,
+			heThey: heTheyCount,
+			sheHer: sheHerCount,
+			sheThey: sheTheyCount,
+			theyThem: theyThemCount,
+			nothing: nothingCount,
+		});
+		return res.send(apiResponse);
+	})  
+	.catch(error => {
+		return res.send(error);
+	});
+});
+
+app.listen(5000, () =>
+  console.log('api listening on port ' + (5000)),
+);
+
+var connect = require('connect');
+var serveStatic = require('serve-static');
+
+connect()
+    .use(serveStatic(__dirname))
+    .listen((process.env.PORT || 80), () => console.log('Server running on ' + (process.env.PORT || 80)));
